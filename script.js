bec3260cc59afa5c9ae6286a879fc21d645380e4
Galileu Rodrigues de Lima <54282757+rlgalileu@users.noreply.github.com>
2022-02-27 21:56:37 -0300

    manipulate DOM with api data and add events to buttons

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterButton = document.querySelector('#twitter');
+const newQuoteButton = document.querySelector('#new-quote');
+
+let apiQuotes = [];
+
 // Show New Quote
 function newQuote() {
   const random = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[random];
 
-  // return quote;
-  console.log(quote)
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 100) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  quoteText.textContent = quote.text;
 }
 
 // Get quotes from API
-let apiQuotes = [];
-
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
   try {
@@ -18,9 +35,19 @@ async function getQuotes() {
 
     newQuote();
   } catch (error) {
-
+    console.log(error)
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteButton.addEventListener('click', newQuote);
+twitterButton.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
